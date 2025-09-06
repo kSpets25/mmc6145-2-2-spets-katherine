1206452fac55ec5c0cec4524628625f87068ba82
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-09-06 13:44:31 -0500

    added time funtions
    added game functions to app file. tests passed but need to check for function.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,36 @@ import Header from "./components/header";
 import Modal from "./components/modal";
 import { useTimer } from "./util/customHooks";
 
+
 export default function App() {
   const [showModal, setShowModal] = useState(false);
+  const {time, start: timerStart, stop: timerStop, reset: timerReset, } = useTimer(0); 
+  const [previousTime, setPreviousTime] = useState(null);
+  const [bestTime, setBestTime] = useState(null);
+  
+  //ToDo Notes:  
+  //need to show previous time/ done
+  //Stop counting and don't show timer/ done
+  //lower time should overwrite previous time /check this.
+  //check game function!!
+  //all tests passed.
 
-  const {
-    time,
-    start: timerStart,
-    stop: timerStop,
-    reset: timerReset,
-  } = useTimer();
+  const startGameTimer= () => {
+    timerReset();
+    timerStart();
+    setPreviousTime()
+  };
+
+  const stopGameTimer = () => {
+    timerStop();
+    setPreviousTime(time);
+    if (bestTime === null || time <bestTime) {setBestTime(time);}
+    timerReset(); 
+  };
+
+  const resetGameTimer = () => {// work on this next?
+    setPreviousTime(time); 
+  }
 
   const cardTexts = [
     "Bunny 🐰",
@@ -27,13 +48,22 @@ export default function App() {
     <>
       <Header
         // add time, bestTime, previousTime props
-        openModal={() => setShowModal(true)}
+       time={time}
+       bestTime={bestTime}
+       previousTime={previousTime}
+       openModal={() => setShowModal(true)}
+
       />
       <CardGame
         // add onGameStart, onGameEnd props
         cardTexts={cardTexts}
+        onGameStart={startGameTimer}
+        onGameEnd={stopGameTimer}
       />
-      <Modal isShown={showModal} close={() => setShowModal(false)} />
+      <Modal isShown={showModal} close={() => setShowModal(false)} 
+        
+      />
+
     </>
   );
 }
